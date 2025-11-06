c22b472522f91a64fca85344700404402823304b
Craig <cban0901@gmail.com>
2025-11-06 14:14:30 +0000

    react router setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,33 @@
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import Root from "./pages/Root"
+import Homepage from "./pages/Homepage"
+import SearchPage from "./pages/SearchPage"
+import DetailsPage from "./pages/DetailsPage.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root/>,
+    children: [
+      {
+        index: true,
+        element: <Homepage/>
+      },
+      {
+        path: "/search",
+        element: <SearchPage/>
+      },
+      {
+        path: "/packages/:name",
+        element: <DetailsPage/>
+      }
+    ]
+  }
+])
+
 function App() {
 
-  return (
-   <div>App</div>
-  )
+  return <RouterProvider router={router}/>
 }
 
 export default App
